c19ab43c37a27a3389461717377cb038ea2a4a42
Patrick Martin <patrickmartin@fastmail.com>
2016-05-19 17:06:55 -0400

    Added static kernel channel names.

---
+++ src/java/edu/hsc/hypower/physicloud/KernelChannels.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package edu.hsc.hypower.physicloud;
+
+/**
+ * This class maintains all kernel channel names across PhysiCloud.
+ * @author pmartin@hsc.edu
+ *
+ */
+public final class KernelChannels {
+
+	public static final String KERNEL = "kernel";
+	public static final String HEARTBEAT = "kernel.hb";
+	public static final String READ_REQUEST = "kernel.readreq";
+	public static final String WRITE_REQUEST = "kernel.writereq";
+	
+}
