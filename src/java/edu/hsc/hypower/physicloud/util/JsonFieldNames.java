fe1ca732b4135ff66fe54e64e4af8c1b8ac5fa9b
benhackley <ben.hackley@gmail.com>
2016-05-26 11:17:58 -0400

    Last push did not seem to pick up JsonFieldNames.java

---
+++ src/java/edu/hsc/hypower/physicloud/util/JsonFieldNames.java
@@ -0,0 +1,12 @@
+package edu.hsc.hypower.physicloud.util;
+
+public final class JsonFieldNames {
+
+	public static final String IP_ADDR = "ipAddr";
+	public static final String SPEED = "proSpeed";
+	public static final String MEMORY = "availMemory";
+	public static final String P_CORES = "pCores";
+	public static final String L_CORES = "lCores";
+	public static final String LOAD = "pLoad";
+
+}
