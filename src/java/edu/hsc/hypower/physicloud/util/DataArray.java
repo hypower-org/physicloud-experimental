37b85ea42ae497e274af97f754bca4e55b4f3063
benhackley <ben.hackley@gmail.com>
2016-07-01 11:16:42 -0400

    Added DataArray class

---
+++ src/java/edu/hsc/hypower/physicloud/util/DataArray.java
@@ -0,0 +1,44 @@
+package edu.hsc.hypower.physicloud.util;
+
+import io.vertx.core.shareddata.Shareable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import edu.hsc.hypower.physicloud.util.*;
+
+public final class DataArray implements Shareable{
+
+	private final ArrayList<DataTuple> dataArray;
+
+	public DataArray(ArrayList<DataTuple> dataArr) {
+		this.dataArray = dataArr;
+	}
+
+
+	public final ArrayList<DataTuple> getData() {
+		return dataArray;
+	}
+
+
+
+	public static void main(String[] args) {
+
+		DataTuple x = new DataTuple("this is a dataTuple", "please work");
+		ArrayList<DataTuple> tupleList = new ArrayList<DataTuple>();
+		tupleList.add(x);
+
+		// Create an actual DataArray
+
+		DataArray arr = new DataArray(tupleList);
+
+		ArrayList<DataTuple> moreTesting = arr.getData();
+
+		for(int i = 0; i < moreTesting.size(); i++)	{
+			System.out.println(moreTesting.get(i).getType());
+			System.out.println(moreTesting.get(i).getData());
+		}
+
+	}
+
+}
