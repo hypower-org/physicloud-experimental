4515bc205cdbe75065c2b4e5b684e6552732a331
Keith <erg@erg-Pangolin-Performance>
2016-07-16 13:30:03 -0400

    Rudimentary interface kit conversion file

---
+++ src/java/edu/hsc/hypower/physicloud/util/IkitConverter.java
@@ -0,0 +1,27 @@
+package edu.hsc.hypower.physicloud.util;
+
+public class IkitConverter {
+	
+	//Temperature
+	
+	public float rawToTemp(int rawValue){
+		float temp = (float) ((rawValue * 0.22222) - 61.11);
+		return temp;
+	}
+	
+	//Humidity
+	
+	public float rawToHumidity(int rawValue){
+		float humid = (float) ((rawValue * 0.1906) - 40.2);
+		return humid;
+	}
+	
+	//Magnetic
+	
+	public float magnetToGauss(int rawValue){
+		float gauss = 500 - rawValue;
+		return gauss;
+	}
+	
+
+}
